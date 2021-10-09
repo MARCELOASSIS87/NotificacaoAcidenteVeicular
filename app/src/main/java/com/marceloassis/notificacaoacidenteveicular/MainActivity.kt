724fd862c05bd42cb82ecf4020c15fc02dfede94
Marcelo Assis <marceloassis50@gmail.com>
2021-10-09 15:45:31 -0300

    Pedido de autorização do uso da localização, e monitoramento da mesma, agora falta o teste

--- app/src/main/java/com/marceloassis/notificacaoacidenteveicular/MainActivity.kt
+++ app/src/main/java/com/marceloassis/notificacaoacidenteveicular/MainActivity.kt
@@ -1,45 +1,207 @@
 package com.marceloassis.notificacaoacidenteveicular
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 import com.marceloassis.notificacaoacidenteveicular.databinding.ActivityMainBinding
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import kotlin.properties.Delegates
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
-    private lateinit var sensorManager: SensorManager
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks, EasyPermissions.RationaleCallbacks {
     private lateinit var binding: ActivityMainBinding
 
+    private val TAG = "MainActivity"
+    private val LOCATION_PERM = 124
+    private var speedUpStartTime = 0L
+    private var speedUpEndTime = 0L
+    private var speedDownStartTime = 0L
+    private var speedDownEndTime = 0L
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-    }
-    override fun onSensorChanged(event: SensorEvent?) {
-        binding.textView2.text = "x = ${event!!.values[0]}\n\n" +
-                "y = ${event.values[1]}\n\n" +
-                "z = ${event.values[2]}"
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
 
+    private var isDone: Boolean by Delegates.observable(false) { property, oldValue, newValue ->
+        if (newValue == true) {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        sensorManager.registerListener(
-            this,
-            sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION),
-            SensorManager.SENSOR_DELAY_NORMAL
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        askForLocationPermission()
+        createLocationRequest()
+
+        locationCallback = object : LocationCallback() {
+
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                if (!isDone){
+                    val speedToInt = locationResult.lastLocation.speed.toInt()
+                    calcSpeed(speedToInt)
+                    binding.velocidadeTv.text = speedToInt.toString()
+                }
+            }
+        }
+    }
+
+    private fun calcSpeed(speed: Int) {
+
+        if (speed >= 10){
+            speedUpStartTime=System.currentTimeMillis()
+            speedDownEndTime=System.currentTimeMillis()
+
+            if (speedDownStartTime != 0L){
+                val speedDownTime=speedDownEndTime - speedUpStartTime
+                binding.testeTv.text = (speedDownTime/1000).toString()
+                speedDownStartTime=0L
+            }
+        }
+        else if (speed>=30){
+            if (speedUpStartTime!= 0L){
+                speedUpEndTime = System.currentTimeMillis()
+                val speedUpTime = speedUpEndTime - speedUpStartTime
+                binding.teste1030tv.text=(speedUpTime/1000).toString()
+                speedUpStartTime = 0L
+            }
+            speedDownStartTime=System.currentTimeMillis()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
         )
     }
-    fun chamaListaSensores(view: View) {
-        val intent = Intent(this,ListadeSensores::class.java)
-        startActivity(intent)
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
 
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest.create().apply {
+            interval = 1000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
 
+    private fun askForLocationPermission() {
+        if (hasLocationPermission()) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            fusedLocationProviderClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
 
+                }
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "É preciso que voce permita o uso da localização e calcule sua velocidade",
+                LOCATION_PERM,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+    private fun hasLocationPermission(): Boolean {
+        return EasyPermissions.hasPermissions(
+            this,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this)
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+            AppSettingsDialog.Builder(this).build().show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE){
+            val yes="Permitido"
+            val no = "Negado"
+            Toast.makeText(this,"onActivityResult",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+    }
+
+    override fun onRationaleAccepted(requestCode: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRationaleDenied(requestCode: Int) {
+        TODO("Not yet implemented")
+    }
 }
+
+
